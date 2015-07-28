68227822d1dd04f5ca5ac2ce2001e50bafa70c73
Eugeny Degtyarjov <degtyarjov@haulmont.com>
2015-07-28 08:59:20 +0000

    PL-5738 Show legend, grid and value in column title in serial chart #PL-5738

--- modules/web/src/com/haulmont/charts/web/ui/JsonChartController.java
+++ modules/web/src/com/haulmont/charts/web/ui/JsonChartController.java
@@ -4,9 +4,9 @@
  */
 package com.haulmont.charts.web.ui;
 
-import com.haulmont.charts.gui.amcharts.model.charts.PieChart;
 import com.haulmont.charts.web.gui.components.charts.amcharts.WebChart;
 import com.haulmont.charts.web.toolkit.ui.amcharts.CubaAmchartsScene;
+import com.haulmont.cuba.gui.WindowParam;
 import com.haulmont.cuba.gui.components.AbstractFrame;
 
 import java.util.Map;
@@ -15,15 +15,16 @@ import java.util.Map;
  * @author degtyarjov
  */
 public class JsonChartController extends AbstractFrame {
+    @WindowParam
+    protected String chartJson;
+
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
-        String chartJson = (String) params.get("Chart");
         WebChart webChart = new WebChart();
         webChart.setHeight("100%");
         webChart.setWidth("100%");
         CubaAmchartsScene cubaAmchartsScene = webChart.getComponent();
-        cubaAmchartsScene.drawChart(new PieChart());
         cubaAmchartsScene.setJson(chartJson);
         cubaAmchartsScene.drawChart();
         add(webChart);
